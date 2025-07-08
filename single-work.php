09c8a34d3aaa39578f1a0d2374f7eed444a3a7fb
ChillibyteUK <dan@chillibyte.co.uk>
2025-07-08 13:53:24 +0100

    single work video top margin

--- single-work.php
+++ single-work.php
@@ -37,7 +37,7 @@ get_header();
 		$video  = get_field( 'video' );
 		$poster = $video['poster'];
 		?>
-		<div class="container video-lightbox-wrapper position-relative">
+		<div class="container video-lightbox-wrapper position-relative mt-5">
 			<picture>
 				<source srcset="<?= esc_attr( wp_get_attachment_image_srcset( $poster, 'full' ) ); ?>" type="image/jpeg">
 				<img 
