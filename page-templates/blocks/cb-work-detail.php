c2137d7cb9b60e83d09b9825e7572afcb9896c54
ChillibyteUK <dan@chillibyte.co.uk>
2025-07-31 17:22:37 +0100

    work detail sticky top

--- page-templates/blocks/cb-work-detail.php
+++ page-templates/blocks/cb-work-detail.php
@@ -11,12 +11,14 @@ defined( 'ABSPATH' ) || exit;
 <div class="container work-detail">
 	<div class="row gx-5">
 		<div class="col-md-6">
-			<?php
-			if ( wp_get_attachment_image( get_field( 'logo', get_the_ID() ), 'full' ) ) {
-				echo wp_get_attachment_image( get_field( 'logo', get_the_ID() ), 'full', false, array( 'class' => 'work-detail__logo' ) );
-			}
-			?>
-			<div><?= wp_kses_post( get_field( 'left_content' ) ); ?></div>			
+			<div class="work-detail__left">
+				<?php
+				if ( wp_get_attachment_image( get_field( 'logo', get_the_ID() ), 'full' ) ) {
+					echo wp_get_attachment_image( get_field( 'logo', get_the_ID() ), 'full', false, array( 'class' => 'work-detail__logo' ) );
+				}
+				?>
+				<div><?= wp_kses_post( get_field( 'left_content' ) ); ?></div>
+			</div>
 		</div>
 		<div class="col-md-6">
 			<div class="work-detail__right">
