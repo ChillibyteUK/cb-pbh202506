c24a2429b2d23511e2cc24258035c07cb47c15a7
ChillibyteUK <dan@chillibyte.co.uk>
2025-09-03 13:50:21 +0100

    scroller space

--- page-templates/blocks/cb-scroller-hero.php
+++ page-templates/blocks/cb-scroller-hero.php
@@ -7,6 +7,7 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
+<div class="py-5 has-light-background-color"></div>
 <div class="scroller-hero">
 	<div class="container px-0">
 		<?= wp_get_attachment_image( get_field( 'image' ), 'full', false, array( 'class' => 'scroller-hero__image' ) ); ?>
