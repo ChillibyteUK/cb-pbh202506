831f4a158eb308fc8bff20559835e2a881fa61ec
ChillibyteUK <dan@chillibyte.co.uk>
2025-09-08 10:25:10 +0100

    legal page template

--- legal-page.php
+++ legal-page.php
@@ -11,16 +11,26 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 
 ?>
-<main id="main" class="<?= esc_attr( $classes ); ?>">
-	<div class="container pt-5">
-		<h1><?= esc_html( get_the_title() ); ?></h1>
-	</div>
-	<div class="container pt-5">
-	<?php
-    the_post();
-    the_content();
-	?>
-	</div>
+<style>
+:root {
+	--bs-border-color: #000;
+}
+</style>
+<main id="main" class="blog blog-single">
+	<section class="blog-single__hero">
+		<div class="container">
+			<h1 class="blog-single__title"><?= esc_html( get_the_title() ); ?></h1>
+		</div>
+	</section>
+	<section class="blog__content">
+		<div class="container pb-5">
+			<div class="row">
+				<div class="col-md-8 offset-md-2">
+					<?= wp_kses_post( get_the_content() ); ?>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
 <?php
 get_footer();
